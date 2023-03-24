ffd9d4f6be5981cb3131f88926d192ca12ddc88c
Alice <alicia.mar.val@gmail.com>
2023-03-24 18:16:33 -0300

    atividade1

--- exe3.java
+++ exe3.java
@@ -2,18 +2,53 @@ import java.util.Random;
 
 public class exe3 {
     public static void main(String [] args) {
-        int dado1,dado2;
-        boolean ganhou = true;
-        int lançamento = 1;
-        int pontos = 0;
+        int dados [] = {0,0};
+        int lancamento = 1;
+        int ponto = 0;
+        boolean ganhou = false;
 
+        dado(dados);
+        System.out.println("Soma: "+(dados[0]+dados[1]));
        
-        
+        if(lancamento == 1){
+            
+            if((dados[0]+dados[1]) == 7 || (dados[0]+dados[1]) == 11 ){
+                ganhou = true;
+                lancamento++;
+            }
+            if((dados[0]+dados[1]) == 2 || (dados[0]+dados[1]) == 3 || (dados[0]+dados[1]) == 12 ){
+                lancamento++;
+            }else{
+                ponto = dados[0]+dados[1];
+                System.out.println("Seu ponto e "+ponto);
+                System.out.println("Iniciando a segunda rodada");
+            }
+        }
+
+        if (ponto != 0 && lancamento == 1) {
+
+            do {
+                dado(dados);
+                System.out.println("Soma: "+(dados[0]+dados[1]));
+            
+                if(dados[0]+dados[1] == ponto){
+                    ganhou = true;
+                }
+            } while ((dados[0]+dados[1]) != 7 && (dados[0]+dados[1]) != ponto);
+        }
+
+        if(ganhou){
+            System.out.println("Voce ganhou!");
+        } else {
+            System.out.println("Voce perdeu!");
+        }
     }
 
-    public static void dados(int dado) {
-        dado = (int) (Math.random()*6)+1;
-        System.out.println("Dado: " + dado);
+    public static void dado(int dados[]) {
+        dados[0] = (int) (Math.random()*6)+1;
+        dados[1] = (int) (Math.random()*6)+1;
+        System.out.println("Dado 1: " + dados[0] + " Dado 2: " + dados[1]);
+        
     }
 
 }
